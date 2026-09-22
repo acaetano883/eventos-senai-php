45c9528749afa821b4821e6792ff36d7db20f5fb
ANT�NIO GABRIEL DO SANTOS CAETANO <26175338@educ123.sp.senai.br>
2026-09-22 17:06:56 -0300

    feat: concerto do cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,31 +1,36 @@
 <?php
 require_once 'init.php';
 
+// Garante que a chave proximo_id existe na sessão
+if (!isset($_SESSION["proximo_id"])) {
+    $_SESSION["proximo_id"] = 1;
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    // Verifica se TODOS os campos obrigatórios foram enviados
     if (
-        !isset($_POST["titulo"]) &&
-        !isset($_POST["descricao"]) &&
-        !isset($_POST["area"]) &&
-        !isset($_POST["data"]) &&
-        !isset($_POST["inicio"]) &&
-        !isset($_POST["fim"]) &&
-        !isset($_POST["local"]) &&
-        !isset($_POST["responsavel"])
+        isset($_POST["titulo"]) &&
+        isset($_POST["descricao"]) &&
+        isset($_POST["area"]) &&
+        isset($_POST["data"]) &&
+        isset($_POST["inicio"]) &&
+        isset($_POST["fim"]) &&
+        isset($_POST["local"]) &&
+        isset($_POST["responsavel"])
     ) {
 
         $id = $_SESSION["proximo_id"];
 
         $_SESSION["eventos"][$id] = [
-            "id" => $id,
-            "titulo" => $_POST["titulo"],
-            "descricao" => $_POST["descricao"],
-            "area" => $_POST["area"],
-            "data" => $_POST["data"],
-            "inicio" => $_POST["inicio"],
-            "fim" => $_POST["fim"],
-            "local" => $_POST["local"],
+            "id"          => $id,
+            "titulo"      => $_POST["titulo"],
+            "descricao"   => $_POST["descricao"],
+            "area"        => $_POST["area"],
+            "data"        => $_POST["data"],
+            "inicio"      => $_POST["inicio"],
+            "fim"         => $_POST["fim"],
+            "local"       => $_POST["local"],
             "responsavel" => $_POST["responsavel"]
         ];
 
